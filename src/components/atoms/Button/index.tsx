aecd58bdc81929def53e62d0dc2a31be006098e6
Aleksey Shirokov <shirokovaloff@gmail.com>
2021-03-30 23:54:48 +0300

    Button and Arrow Draft

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Button } from './styled'
+import Arrow, { ArrowDirection } from '../Icon/Arrow'
 
-interface IProps {}
+export enum ButtonSize {
+  S,
+  L,
+  XL,
+}
 
-const MainButton: React.FunctionComponent<IProps> = () => {
-  return <div></div>
+interface IProps {
+  size?: ButtonSize
+  arrow?: ArrowDirection
+}
+
+const MainButton: React.FunctionComponent<IProps> = ({
+  children,
+  size,
+  arrow,
+}) => {
+  const renderWithArrows = (arrowDirection: ArrowDirection) => {
+    return arrowDirection === ArrowDirection.LEFT ? (
+      <>
+        <Arrow direction={arrowDirection} />
+        {children}
+      </>
+    ) : (
+      <>
+        {children}
+        <Arrow direction={arrowDirection} />
+      </>
+    )
+  }
+
+  return (
+    <Button size={size}>
+      {arrow ? renderWithArrows(arrow) : { children }}
+    </Button>
+  )
 }
 
 export default MainButton
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MainButton, { ButtonSize } from './components/atoms/Button'
+import { ArrowDirection } from './components/atoms/Icon/Arrow'
 
 const Root = () => {
-  return <div>Rootasdas</div>
+  return (
+    <MainButton size={ButtonSize.XL} arrow={ArrowDirection.LEFT}>
+      Rootasdas
+    </MainButton>
+  )
 }
 
 export default ReactDOM.render(<Root />, document.querySelector('#root'))
