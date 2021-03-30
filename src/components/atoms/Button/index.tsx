bef887939d414d0d7f77fd3cafec2ce61dc68d32
Aleksey Shirokov <shirokovaloff@gmail.com>
2021-03-30 22:51:21 +0300

    Initial commit

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface IProps {}
+
+const MainButton: React.FunctionComponent<IProps> = () => {
+  return <div></div>
+}
+
+export default MainButton
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Root = () => {
+  return <div>Rootasdas</div>
+}
+
+export default ReactDOM.render(<Root />, document.querySelector('#root'))
