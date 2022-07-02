c76233be1d882597bc1d0198a4418940538cd42a
Aleksey Shirokov <ashirokov@artec3d.com>
2022-07-02 14:24:32 +0200

    Fix storybook resolving absolute paths bug for ts modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,24 @@
+const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ['../app/**/stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-actions'
   ],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-webpack5',
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
   },
 };
